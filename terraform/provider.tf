09f69c5e5bc45b8dd7924c6cdadb084670b20c25
BhattKanishk <133306721+BhattKanishk@users.noreply.github.com>
2023-06-12 12:42:04 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIAWH35LBYSNWTIMXUO"
-  secret_key = "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"
+  access_key = "Access_ky"
+  secret_key = "secret_key"
 }
