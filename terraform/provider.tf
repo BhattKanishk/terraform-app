7b0c59a13e5ca00c74c1794cae251b457cfaaef4
BhattKanishk <133306721+BhattKanishk@users.noreply.github.com>
2023-06-14 10:32:44 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,16 @@ provider "aws" {
   access_key = "Access_ky"
   secret_key = "secret_key"
 }
+terraform {
+  backend "s3" {
+    bucket                  = "newbucketterraform23"
+    key                     = "myapp002.tfstate"
+    region                  = "eu-west-1"
+    shared_credentials_file = "C:\\Users\\KANISHK BHATT\\.aws\\credentials"
+  }
+}
+
+provider "aws" {
+  region                  = "eu-west-1"
+  shared_credentials_files = ["C:\\Users\\KANISHK BHATT\\.aws\\credentials"]
+}
