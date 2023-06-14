9c8c7a9503a1aa78474a252e011a231a8e6bff3c
BhattKanishk <133306721+BhattKanishk@users.noreply.github.com>
2023-06-14 10:28:44 +0530

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "s3" {
+  bucket = "${var.app_name}-s3bucket"
 
   tags = {
-    Name        = var.s3_tag
-    Environment = "Dev"
+    Name        = var.app_name
+    Environment = var.env
   }
 }
 resource "aws_instance" "myfirstserver" {
   ami           = "ami-01dd271720c1ba44f"
   instance_type = "t2.micro"
-  key_name = var.key
+  key_name = var.app_name
   tags = {
-    Name = var.instance_tag
+    Name = var.app_name
   }
 }
 
